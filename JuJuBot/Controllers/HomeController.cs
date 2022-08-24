46b285a7bc930f6aa0a72289e0c301190027ddfb
w4560000 <39783609+w4560000@users.noreply.github.com>
2022-08-24 14:05:14 +0800

    新增 Three.js Sample

--- JuJuBot/Controllers/HomeController.cs
+++ JuJuBot/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using JuJuBot.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace JuJuBot.Controllers
 {
@@ -21,17 +15,6 @@ namespace JuJuBot.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
